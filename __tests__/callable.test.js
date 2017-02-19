4dda232f9a1b60fbf5a8a2b07db2cc7751f0f6dc
theory friction practice <jackson.brebnor@gmail.com>
2017-02-18 21:54:43 -0500

    callOnCallBin

--- __tests__/callable.test.js
+++ __tests__/callable.test.js
@@ -1,10 +1,21 @@
 import 'jasmine-expect';
 import { callBin, callOn, isFunc, pipeline, } from 'src/callable';
+
 describe('callable', () => {
   describe('isFunc', () => {
     it('checks if the argument is a function', () => {
       expect(isFunc(x => x)).toBeTrue();
       expect(isFunc(3)).toBeFalse();
+    });
+  });
+  describe('callOn', () => {
+    it('invokes a function on an argument', () => {
+      expect(callOn(2)((x => x))).toEqual(2);
+    });
+  });
+  describe('callBin', () => {
+    it('invokces a function with an argument', () => {
+      expect(callBin(2, x => x)).toEqual(2);
     });
   });
 });
